4b95f12389f9a18cf9b9e91e4fddbe1f976773fb
namndtth <namnd.tth@gmail.com>
2025-03-09 00:17:52 +0700

    1. C2. Skibidus and Fanum Tax (hard version)

--- codeforces/2065C2.cpp
+++ codeforces/2065C2.cpp
@@ -1,3 +1,10 @@
+/// Note: we can choose j repeatedly
+/// Insert -INF into `a` before a.begin()
+/// Sort `b` in asc order
+/// a[i] = b[j] - a[i] and a[i - 1] <= a[i] => a[i - 1] <= b[j] - a[i] => a[i - 1] + a[i] <= b[j]
+/// => binary search in b the smallest element that satisfies greater or equal a[i - 1] + a[i]
+// There are four cases. Each case is handled and explained as below.
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -7,27 +14,37 @@ void solve() {
 
     vector<int> a(n), b(m);
     for (int &val : a) cin >> val;
+    a.insert(a.begin(), numeric_limits<int>::min());
     for (int &val : b) cin >> val;
 
     sort(b.begin(), b.end());
 
-    bool is_non_desc{true};
-    int i = n - 1, j = m - 1;
-    while (i >= 0 && j >= 0) {
-        if (a[i] < b[j] - a[i]) {
-            a[i] = b[j] - a[i];
-            --j;
+    for (int i = 1; i <= n; ++i) {
+        auto lower = lower_bound(b.begin(), b.end(), a[i - 1] + a[i]);
+        if (lower == b.end()) {
+            // lower == b.end() && a[i - 1] > a[i] => no output
+            if (a[i - 1] > a[i]) {
+                cout << "NO" << endl;
+                return;
+            }
+            // lower == b.end() && a[i - 1] <= a[i] => a[i] unchanged
+        } else {
+            // lower != b.end && a[i - 1] > a[i] => we only can choose *lower - a[i]
+            // lower != b.end && a[i - 1] <= a[i] => take min value
+            a[i] = a[i - 1] > a[i] ? *lower - a[i] : min(a[i], *lower - a[i]);
         }
-        if (a[i] > a[i + 1]) {
-            is_non_desc = false;
-            break;
-        }
-        --i;
     }
-    cout << (is_non_desc ? "YES" : "NO") << endl;
+
+    cout << "YES" << endl;
 }
 
 int main() {
-    solve();
+    int tc;
+    cin >> tc;
+
+    while (tc--) {
+        solve();
+    }
+
     return EXIT_SUCCESS;
 }
